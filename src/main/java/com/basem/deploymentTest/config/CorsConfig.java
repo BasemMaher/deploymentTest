e3b69370b6f3f5068494a6986f8d49e34d04efeb
basem maher <basem.maher109@gmail.com>
2025-05-04 16:40:19 +0300

    fix cors configeration V2

--- src/main/java/com/basem/deploymentTest/config/CorsConfig.java
+++ src/main/java/com/basem/deploymentTest/config/CorsConfig.java
@@ -14,7 +14,7 @@ public class CorsConfig {
             @Override
             public void addCorsMappings(CorsRegistry registry) {
                 registry.addMapping("/**")
-                        .allowedOrigins("https://gangoffivefront.up.railway.app")
+                        .allowedOriginPatterns("*")
                         .allowedMethods("GET", "POST", "PUT", "PATCH", "DELETE", "OPTIONS")
                         .allowedHeaders("*")
                         .allowCredentials(true);
